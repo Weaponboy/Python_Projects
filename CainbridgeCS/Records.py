0d5d6c8ca8a77ad245acd2e8db07731d523120d3
Weaponboy <joshandy06@gmail.com>
2025-03-25 15:21:06 +0200

    Record idea

---
+++ CainbridgeCS/Records.py
@@ -0,0 +1,13 @@
+
+class Student :
+    Name = ""
+    Birthday = ""
+    Class = ""
+    Gender = ""
+
+myStudent = Student()
+
+myStudent.Name = "Ahmed Sayed"
+myStudent.Birthday = "Ahmed Sayed"
+
+
